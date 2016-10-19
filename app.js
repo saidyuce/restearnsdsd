e6f1005f2d9bdbc5587a246b38abab1a4817b569
saidyuce <saidyuce94@gmail.com>
2016-10-20 01:17:06 +0300

    Update app.js

--- app.js
+++ app.js
@@ -70,7 +70,22 @@ io.on('connection', function(socket) {
                 }
             }
         }
+else if (tip == "said1234oturum") {
 
+            var my_cl_obj = new Object();
+            my_cl_obj.data = json_message;
+            my_cl_obj.con = socket;
+	        console.log(client_sockets.length);
+            for (var i6 = 0; i6 < client_sockets.length; ++i6) {
+		    console.log('User id:');
+			console.log(client_sockets[i6].data.user_id);
+                if (client_sockets[i6].data.user_id == my_cl_obj.data.user_id) {
+			
+            client_sockets[i6].con.emit(client_sockets[i6].data.user_id + "_oturum_geri_bildirim",my_cl_obj.data );
+			console.log(client_sockets[i6].data.user_id+"");
+                }
+            }
+        }
         console.log('New Chat Message ', msg);
         io.sockets.emit('txt', msg);
     });
